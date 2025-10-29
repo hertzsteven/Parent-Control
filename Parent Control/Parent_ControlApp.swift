bda7bff3632f473a27406099daf6029390f26c26
Steven Hertz <StevenH@appi4.com>
2025-10-28 22:49:38 -0400

    Initial Commit

---
+++ Parent Control/Parent_ControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  Parent_ControlApp.swift
+//  Parent Control
+//
+//  Created by Steven Hertz on 10/28/25.
+//
+
+import SwiftUI
+
+@main
+struct Parent_ControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
